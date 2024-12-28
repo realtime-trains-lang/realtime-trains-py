36fad0d5dfc4f4cbf307501b88cb1ff513196a4e
anonymous44401 <anonymous4401.ad@gmail.com>
2024-12-28 21:18:15 +0000

    docs: updated documentation for methods

--- src/realtime_trains_py/__init__.py
+++ src/realtime_trains_py/__init__.py
@@ -38,70 +38,80 @@ class RealtimeTrainsPy():
 
     def get_departures(self, tiploc: str, filter: str = None, date: str = None, rows: int = None, time: str = None) -> list | str:
         """
-        ### Parameters
-        **tiploc** *(required)*
+        ## Parameters
+
+        ### tiploc (required)
 
             A string representing the Timing Point Location Code (TIPLOC) or Computer Reservation Code (CRS) of the station.
 
-        **filter** *(optional)*
+        ### filter (optional)
             A string representing the Timing Point Location Code (TIPLOC) or Computer Reservation Code (CRS) of the filter station.
 
-        **date** *(optional)*
+        ### date (optional)
             A string representing the date in the format YYYY/MM/DD.
 
-        **rows** *(optional)*
+        ### rows (optional)
             An integer representing the maximum number of rows to return. (Only available for simple and advanced complexity.)
 
-        **time** *(optional)*
+        ### time (optional)
             A string representing the time in the format HHMM. 
 
-
-        ### Example data
+        ### Examples
         ```python
-        get_departures(tiploc = "KNGX", filter = "STEVNGE", date = "2024/11/16", time = "1800", rows = 10)
+        get_station(tiploc = "KNGX", filter = "STEVNGE", date = "2024/11/16", time = "1800", rows = 10)
+
+        get_station(tiploc = "YORK", date = "2024/11/16", time = "1800")
         ```
         """
         return self.__boards._get_dep_board_details(tiploc = tiploc, filter = filter, date = date, rows = rows, time = time)
 
     def get_arrivals(self, tiploc: str, filter: str = None, date: str = None, rows: int = None, time: str = None) -> list | str:
         """
-        **tiploc** *(required)*
+        ## Parameters
+
+        ### tiploc (required)
 
             A string representing the Timing Point Location Code (TIPLOC) or Computer Reservation Code (CRS) of the station.
 
-        **filter** *(optional)*
+        ### filter (optional)
             A string representing the Timing Point Location Code (TIPLOC) or Computer Reservation Code (CRS) of the filter station.
 
-        **date** *(optional)*
+        ### date (optional)
             A string representing the date in the format YYYY/MM/DD.
 
-        **rows** *(optional)*
+        ### rows (optional)
             An integer representing the maximum number of rows to return. (Only available for simple and advanced complexity.)
 
-        **time** *(optional)*
+        ### time (optional)
             A string representing the time in the format HHMM. 
 
-        ### Example data
+        ### Examples
         ```python
-        get_arrivals(tiploc = "KNGX", filter = "STEVNGE", date = "2024/11/16", time = "1800", rows = 10)
+        get_station(tiploc = "KNGX", filter = "STEVNGE", date = "2024/11/16", time = "1800", rows = 10)
+
+        get_station(tiploc = "YORK", date = "2024/11/16", time = "1800")
         ```
         """
         return self.__boards._get_arr_board_details(tiploc = tiploc, filter = filter, date = date, rows = rows, time = time)
 
     def get_service(self, service_uid: str, date: str = None) -> list | str:
         """
-        **service_uid** *(required)*
+        ## Parameters
+
+        ### service_uid (required)
             A string representing the Service Unique Identity (UID) code.
 
-        **date** *(optional)*
+        ### date (optional)
             A string representing the date in the format YYYY/MM/DD.
 
-        **time** *(optional)*
+        ### time (optional)
             A string representing the time in the format HHMM. 
 
-        ### Example data
+        ### Examples
         ```python
         get_service(service_uid = "G54071", date = "2024/11/16", time = "1800")
+
+        get_service(service_uid = "G26171")
         ```
         """
         return self.__services._get_service_details(service_uid = service_uid, date = date)
@@ -110,25 +120,27 @@ class RealtimeTrainsPy():
         """
         ## This feature is only available for complex mode
 
-        **tiploc** *(required)*
+        ### tiploc (required)
 
             A string representing the Timing Point Location Code (TIPLOC) or Computer Reservation Code (CRS) of the station.
 
-        **filter** *(optional)*
+        ### filter (optional)
             A string representing the Timing Point Location Code (TIPLOC) or Computer Reservation Code (CRS) of the filter station.
 
-        **date** *(optional)*
+        ### date (optional)
             A string representing the date in the format YYYY/MM/DD.
 
-        **rows** *(optional)*
+        ### rows (optional)
             An integer representing the maximum number of rows to return. (Only available for simple and advanced complexity.)
 
-        **time** *(optional)*
+        ### time (optional)
             A string representing the time in the format HHMM. 
 
-        ### Example data
+        ### Examples
         ```python
         get_station(tiploc = "KNGX", filter = "STEVNGE", date = "2024/11/16", time = "1800", rows = 10)
+
+        get_station(tiploc = "YORK", date = "2024/11/16", time = "1800")
         ```
         """
         return self.__boards._get_stat_board_details(tiploc = tiploc, filter = filter, date = date, rows = rows, time = time)
