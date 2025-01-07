7b2fa414f8a7732e3a846a06a87aa67f86d506de
anonymous44401 <anonymous4401.ad@gmail.com>
2025-01-07 17:57:02 +0000

    chore: added comments and updated formatting

--- src/realtime_trains_py/services/board_data.py
+++ src/realtime_trains_py/services/board_data.py
@@ -169,7 +169,7 @@ class Boards():
                 return "Departure board printed successfully" 
 
             # Advanced/Simple (Normal)
-            elif self.__complexity == "a.n" or self.__complexity == "s.n":
+            elif self.__complexity.endswith("n"):
                 # Create a new departure board list
                 departure_board: list = []
                 
@@ -315,7 +315,6 @@ class Boards():
 
                 for service in service_data["services"]:
                     location_detail = service["locationDetail"] # Details of the location
-                    destinations = location_detail["destination"] # Destinations of services
                     status = location_detail["displayAs"] # Status of service
 
                     # Check if booked arrival is in location detail
@@ -387,7 +386,7 @@ class Boards():
                 return "Arrivals board printed successfully"
             
             # Advanced/Simple (Normal)
-            elif self.__complexity == "a.n" or self.__complexity == "s.n":
+            elif self.__complexity.endswith("n"):
                 # Create a new arrivals board list
                 arrivals_board: list = []
                 
@@ -396,7 +395,6 @@ class Boards():
 
                 for service in service_data["services"]:
                     location_detail = service["locationDetail"] # Details of the location
-                    destinations = location_detail["destination"] # Destinations of services
                     status = location_detail["displayAs"] # Status of service
 
                     # Check if booked arrival is in location detail
@@ -577,111 +575,121 @@ class Boards():
             raise Exception(f"Failed to connect to the RTT API server. Try again in a few minutes. Status codes {dep_api_response.status_code} {arr_api_response.status_code}")
 
     def __create_dep_adv_service(self, service):
-        location_detail = service["locationDetail"]
-        destination = location_detail["destination"]
-        status = location_detail["displayAs"]
+        location_detail = service["locationDetail"] # Details of the location
+        status = location_detail["displayAs"] # Status of service
 
-
+        # Check if booked departure is in location detail
         if "gbttBookedDeparture" in location_detail:
             gbtt_departure = location_detail["gbttBookedDeparture"]
-
         else:
-            gbtt_departure = "Unknown"
+            gbtt_departure = ""
 
+        # Check if platform is in location detail
         if "platform" in location_detail:
             platform = location_detail["platform"]
 
         else:
-            platform = "Unknown"
+            platform = "-"
 
+        # Check if realtime departure is in location detail
         if "realtimeDeparture" in location_detail:
             realtime_departure = location_detail["realtimeDeparture"]
 
         else:
-            realtime_departure = "Unknown"
+            realtime_departure = "-"
 
+        # Check if service UID is in location detail
         if "serviceUid" in service:
             service_uid = service["serviceUid"]
 
         else:
-            service_uid = "Unknown"
+            service_uid = "-"
 
 
+        # Check if the status isn't cancelled
         if status != "CANCELLED_CALL":
+            # If the gbtt departure and realtime departure are equal, set realtime departure to On Time
             if gbtt_departure == realtime_departure:
                 realtime_departure = "On time"
-                gbtt_departure = format_time(gbtt_departure)
 
-            elif realtime_departure == "Unknown":
-                gbtt_departure = format_time(gbtt_departure)
-
-            else:
-                realtime_departure = format_time(realtime_departure)
-                realtime_departure = "Exp " + realtime_departure
-                gbtt_departure = format_time(gbtt_departure)
+            # If the realtime departure isn't null, format and add Exp
+            elif realtime_departure != "-":
+                realtime_departure = f"Exp {format_time(realtime_departure)}"
+            
+            # Format the gbtt departure
+            gbtt_departure = format_time(gbtt_departure)
 
         else:
+            # Set the realtime departure to cancelled
             realtime_departure = "Cancelled"
+            # Format the gbtt departure
             gbtt_departure = format_time(gbtt_departure)
 
         
-        terminus = destination.pop()["description"]
+        # Pop the terminus
+        terminus = (location_detail["destination"]).pop()["description"]
 
+        # Return the service UID and Departure Board 
         return service_uid, DepartureBoard(gbtt_departure, terminus, platform, realtime_departure, service_uid)
 
     def __create_arr_adv_service(self, service):
-        location_detail = service["locationDetail"]
-        destinations = location_detail["destination"]
-        origins = location_detail["origin"]
-        status = location_detail["displayAs"]
+        location_detail = service["locationDetail"] # Details of the location
+        status = location_detail["displayAs"] # Status of service
 
+        # Check if booked arrival is in location detail
         if "gbttBookedArrival" in location_detail:
             gbtt_arrival = location_detail["gbttBookedArrival"]
 
         else:
-            gbtt_arrival = "Unknown"
+            gbtt_arrival = "-"
 
+        # Check if platform is in location detail
         if "platform" in location_detail:
             platform = location_detail["platform"]
 
         else:
-            platform = "Unknown"
+            platform = "-"
 
+        # Check if realtime arrival is in location detail
         if "realtimeArrival" in location_detail:
             realtime_arrival = location_detail["realtimeArrival"]
 
         else:
-            realtime_arrival = "Unknown"
+            realtime_arrival = "-"
 
+        # Check if service UID is in location detail
         if "serviceUid" in service:
             service_uid = service["serviceUid"]
 
         else:
-            service_uid = "Unknown"
+            service_uid = "-"
 
-
+        # Check if the status isn't cancelled
         if status != "CANCELLED_CALL":
+            # If the gbtt arrival and realtime arrival are equal, set realtime arrival to On Time
             if gbtt_arrival == realtime_arrival:
                 realtime_arrival = "On time"
-                gbtt_arrival = format_time(gbtt_arrival)
 
-            elif realtime_arrival == "Unknown":
-                gbtt_arrival = format_time(gbtt_arrival)
-
-            else:
-                realtime_arrival = format_time(realtime_arrival)
-                realtime_arrival = "Exp " + realtime_arrival
-                gbtt_arrival = format_time(gbtt_arrival)
-
-        else:
-            realtime_arrival = "Cancelled"
+            # If the realtime arrival isn't null, format and add Exp
+            elif realtime_arrival != "-":
+                realtime_arrival = "Exp " + format_time(realtime_arrival)
+            
+            # Format the gbtt arrival
             gbtt_arrival = format_time(gbtt_arrival)
 
-        for destination in destinations:
-            terminus = destination["description"]
-        
-        origin = origins.pop()["description"]
+        else:
+            # Set the realtime arrival to cancelled
+            realtime_arrival = "Cancelled"
+            # Format the gbtt arrival
+            gbtt_arrival = format_time(gbtt_arrival)
 
+        # Pop the terminus
+        terminus = (location_detail["destination"]).pop()["description"]
+        
+        # Pop the origin
+        origin = (location_detail["origin"]).pop()["description"]
+
+        # Return the service UID and Arrival Board 
         return service_uid, ArrivalBoard(gbtt_arrival, terminus, origin, platform, realtime_arrival, service_uid)
     
     def __create_dep_sim_service(self, service):
