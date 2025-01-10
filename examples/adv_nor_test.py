be5c04a11dae12bf041fd59ddf91361073f72bc2
anonymous44401 <anonymous4401.ad@gmail.com>
2025-01-10 21:37:34 +0000

    chore: added more examples

--- examples/adv_nor_test.py
+++ examples/adv_nor_test.py
@@ -22,6 +22,8 @@ for arrivals in arrivals_at_norwich:
 # G27187 | 21:36 | Cambridge to Norwich
 # G27471 | 21:49 | London Liverpool Street to Norwich
 
+
+
 #### EXAMPLE 2 ####
 
 # Query for getting the next 10 departures from Hull around 10:00 UTC on 10th January 2025
@@ -41,3 +43,29 @@ for departures in departures_at_hull:
 # N35979 | 10:34 | Hull to London Kings Cross
 # W49960 | 10:47 | Hull to York
 # W47603 | 10:50 | Hull to Doncaster
+
+
+
+#### EXAMPLE 3 ####
+
+# Query for getting the details of the service with service_uid "W47587" 
+service = rtt.get_service("W47587")
+
+print(f"Service: {service.train_id} ({service.service_uid})\n{service.origin} to {service.destination} \nOperated by {service.operator}")
+for calling_point in service.calling_points:
+    print(f"Arrive: {calling_point.booked_arrival} Depart: {calling_point.booked_departure} | {calling_point.stop_name} Platform {calling_point.platform}")
+
+### EXAMPLE OUTPUT ###
+# Service: 2C56 (W47587)
+# Hull to Doncaster
+# Operated by Northern
+# Arrive:  Depart: 09:50 | Hull Platform 1
+# Arrive: 09:57 Depart: 09:57 | Hessle Platform 1
+# Arrive: 10:02 Depart: 10:02 | Ferriby Platform 2
+# Arrive: 10:06 Depart: 10:07 | Brough Platform 2
+# Arrive: 10:14 Depart: 10:14 | Gilberdyke Platform 2
+# Arrive: 10:23 Depart: 10:24 | Goole Platform 2
+# Arrive: 10:33 Depart: 10:33 | Thorne North Platform 1
+# Arrive: 10:39 Depart: 10:40 | Hatfield & Stainforth Platform 1
+# Arrive: 10:44 Depart: 10:44 | Kirk Sandall Platform 1
+# Arrive: 10:53 Depart:  | Doncaster Platform 1
